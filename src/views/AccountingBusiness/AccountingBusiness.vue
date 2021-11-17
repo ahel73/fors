5de58b29ca318c2a03afdcd21301525bbc269474
inesa_rymko <inesa_rymko@senla.eu>
2021-11-17 17:43:33 +0300

    MCXMODULEXT-24 Web. Разработка функционала для ведения Учетного дела

---
+++ src/views/AccountingBusiness/AccountingBusiness.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="accountingBusiness">
+    <accountingBusiness-list v-if="$route.path === '/accountingBusiness'" />
+    <router-view />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import AccountingBusinessList from '@/components/AccountingBusiness/AccountingBusinessList.vue';
+import { useStore } from 'vuex-simple';
+import Store from '@/store/store';
+
+@Component({
+  name: 'accountingBusiness',
+  components: {
+    AccountingBusinessList,
+  },
+})
+
+export default class AccountingBusiness extends Vue {
+  store: Store = useStore(this.$store);
+
+  itemIdToDelete: number | null = null;
+  deleteDialog = false;
+}
+</script>
+
+<style scoped lang="scss">
+  .table-action__wrapper {
+    width: 28px;
+  }
+</style>
