4bb523611c08db6da8e29375f4a6ec93c242e2e1
inesa_rymko <inesa_rymko@senla.eu>
2021-11-23 17:13:13 +0300

    MCXMODULEXT-24 Web. Разработка функционала для ведения Учетного дела

--- src/components/AccountingBusiness/AccountingBusinessCard.vue
+++ src/components/AccountingBusiness/AccountingBusinessCard.vue
@@ -27,25 +27,31 @@
               v-model="info.name"
               label="Заявитель"
             />
+          </v-col>
+          <v-col cols="12">
             <select-component
               v-model="info"
               label="Способ УЖУ"
             />
+          </v-col>
+          <v-col cols="12">
             <select-component
               v-model="info.funds"
               label=" Направление расходования средств"
             />
+          </v-col>
+          <v-col cols="12">
             <select-component
               v-model="info.measure.name"
               label=" Сфера занятости"
             />
+          </v-col>
+          <v-col cols="12">
             <select-component
               v-model="info.measure.kbk"
               label="Приоритет"
             />
           </v-col>
-        </v-row>
-        <v-row>
           <v-col cols="6">
             <input-component
               v-model="info.dateCreate"
@@ -87,10 +93,14 @@
         <v-row>
           <v-col cols="12">
             <select-component label=" Период" />
+          </v-col>
+          <v-col cols="12">
             <input-component
               v-model="info.dateCreate"
               label="Дата создания"
             />
+          </v-col>
+          <v-col cols="12">
             <select-component label="Статус" />
           </v-col>
           <v-col />
@@ -128,6 +138,41 @@
         class="tab-card"
         key="family"
       >
+        <v-col>
+          <data-table
+            :headers="headersFamily"
+            :items="form.familyMembers"
+            hide-footer="true"
+          >
+            <template #[`item.actions`]="{ item }">
+              <div class="d-flex justify-center flex-nowrap">
+                <span class="table-action__wrapper">
+                  <router-link
+                    :to="{
+                      name: 'accountingBusiness-card',
+                      params: { id: item.id, type: 'edit' },
+                    }"
+                  >
+                    <base-action
+                      @click="handleOpenMeasure({ id: item.id })"
+                      hint="Редактировать"
+                    >
+                      <edit-icon />
+                    </base-action>
+                  </router-link>
+                </span>
+                <span class="table-action__wrapper">
+                  <base-action
+                    @click="handleDelete()"
+                    hint="Удалить"
+                  >
+                    <delete-icon />
+                  </base-action>
+                </span>
+              </div>
+            </template>
+          </data-table>
+        </v-col>
         <v-row>
           <v-col cols="12">
             <input-component
@@ -179,6 +224,41 @@
         class="tab-card"
         key="document"
       >
+        <v-col>
+          <data-table
+            :headers="headers"
+            :items="form.documents"
+            hide-footer="true"
+          >
+            <template #[`item.actions`]="{ item }">
+              <div class="d-flex justify-center flex-nowrap">
+                <span class="table-action__wrapper">
+                  <router-link
+                    :to="{
+                      name: 'accountingBusiness-card',
+                      params: { id: item.id, type: 'edit' },
+                    }"
+                  >
+                    <base-action
+                      @click="handleOpenMeasure({ id: item.id })"
+                      hint="Редактировать"
+                    >
+                      <edit-icon />
+                    </base-action>
+                  </router-link>
+                </span>
+                <span class="table-action__wrapper">
+                  <base-action
+                    @click="handleDeleteUD(item.id)"
+                    hint="Удалить"
+                  >
+                    <delete-icon />
+                  </base-action>
+                </span>
+              </div>
+            </template>
+          </data-table>
+        </v-col>
         <v-row>
           <v-col cols="12">
             <input-component
@@ -280,6 +360,20 @@
         </router-link>
       </v-col>
     </v-row>
+    <dialog-component
+      v-model="isDeleteMeasureDialogShow"
+      @onSuccess="handleDeleteMeasureSuccess"
+      cancel-title="Отменить"
+      confirm-title="Продолжить"
+      width="600"
+      prompt
+    >
+      <template #title>
+        <text-component variant="h4">
+          Запись будет удалена. Вы уверены?
+        </text-component>
+      </template>
+    </dialog-component>
   </main-layout>
 </template>
 
@@ -296,6 +390,12 @@ import Datepicker from '../shared/Datepicker/Datepicker.vue';
 import CheckboxComponent from '../shared/inputs/CheckboxComponent.vue';
 import MainLayout from '@/layouts/MainLayout.vue';
 import UploadFileComponent from '../shared/inputs/UploadFileComponent.vue';
+import DataTable from '../shared/table/DataTable.vue';
+import DeleteIcon from '@/components/shared/IconComponent/icons/DeleteIcon.vue';
+import EditIcon from '@/components/shared/IconComponent/icons/EditIcon.vue';
+import EyeIcon from '@/components/shared/IconComponent/icons/EyeIcon.vue';
+import BaseAction from '@/components/shared/table/RowActions/BaseAction.vue';
+import TextComponent from '@/components/shared/Text/Text.vue';
 
 @Component({
   components: {
@@ -309,12 +409,19 @@ import UploadFileComponent from '../shared/inputs/UploadFileComponent.vue';
     MainLayout,
     PdfView,
     UploadFileComponent,
+    DataTable,
+    DeleteIcon,
+    EditIcon,
+    EyeIcon,
+    BaseAction,
+    TextComponent,
   },
 })
 
 export default class AccountingBusinessListCard extends Vue {
   isSignatureModalOpen = false;
   isPreviewPrintModalOpen = false;
+  isDeleteMeasureDialogShow = false;
 
   tab = '';
   info = {
@@ -334,25 +441,203 @@ export default class AccountingBusinessListCard extends Vue {
     subsidy: {},
   };
 
-  putOnRecord() {
-    console.log('goToConclusionsRegistry/*  */');
-  }
+  form = {
+    id: 10,
+    regDate: '2011-11-11',
+    area: 33,
+    applicantId: 1,
+    improvingWay: {
+      id: 1,
+      code: '1  ',
+      name: 'социальная выплата',
+    },
+    employment: {
+      id: 1,
+      code: '1',
+      name: 'АПК/Ветеринария',
+    },
+    queuePriority: {
+      id: 1,
+      code: '1',
+      name: 'Многодетная семья',
+    },
+    familyMembers: [
+      {
+        id: 5,
+        individualPersonId: 9,
+        fullName: 'Nest Test',
+        deedId: 10,
+        relationship: 'брат-3',
+        changeUser: null,
+        createUser: null,
+        changeDate: '2021-11-22T17:00:19.10433',
+        createDate: '2021-11-22T16:59:17.515844',
+        identityDoc: {
+          id: 3,
+          seriesNum: '2345532',
+          issueDate: '2010-12-10',
+          authority: 'some_authority',
+          typeId: 2,
+          typeName: 'свидетельство о рождении',
+        },
+        individualPerson: {
+          id: 9,
+          surname: 'testSurname3',
+          name: 'testName3',
+          fullName: 'testSurname3 testName3',
+          patronymic: 'testPatronymi21',
+          birthDate: '2010-11-11',
+        },
+      },
+    ],
+    documents: [
+      {
+        id: 8,
+        deedId: 10,
+        estateObjectId: null,
+        name: 'Название2',
+        docNum: '1212',
+        docDate: '2011-11-11',
+        note: 'note',
+        docEndDate: '2011-11-12',
+        docGroup: {
+          id: 1,
+          name: 'Постановка на учет',
+        },
+        createDate: '2021-11-22T16:49:59.348881',
+        changeDate: '2021-11-22T17:00:19.157283',
+        createUser: null,
+        changeUser: null,
+      },
+    ],
+    spendingDirection: {
+      id: 1,
+      code: '1',
+      name: 'строительство',
+    },
+    oktmo: {
+      id: 1,
+      code: '1',
+      name: '1',
+      areaName: '1',
+      createDate: null,
+      updateDate: null,
+      regioncode: null,
+      areacode: null,
+      citycode: null,
+      localcode: null,
+      controlnum: null,
+      section: null,
+      clarification: null,
+      lastChangeNum: null,
+      lastChangeType: null,
+    },
+    createDate: '2021-11-22T16:49:59.124129',
+    createUser: null,
+    changeUser: null,
+    changeDate: '2021-11-22T17:00:18.942053',
+    changeReason: null,
+    lkSubId: 1,
+    status: {
+      id: 1,
+      name: 'Черновик',
+    },
+  };
 
-  onDeregister() {
-    console.log('goToConclusionsRegistry/*  */');
-  }
+  headersFamily = [
+    {
+      text: '№ п/п',
+      value: 'improving_zu',
+    },
+    {
+      text: 'Фамилия Имя Отчество',
+      value: 'individualPerson.fullName',
+    },
+    {
+      text: 'Дата рождения',
+      value: 'individualPerson.birthDate',
+    },
+    {
+      text: 'Родственные отношения',
+      value: 'relationship',
+    },
+    {
+      text: 'Документ удостоверяющий личность',
+      value: 'identityDoc.typeName',
+    },
+    {
+      text: 'Действия',
+      value: 'actions',
+    },
+  ];
 
-  saveDataApplicant() {
-    console.log('goToConclusionsRegistry/*  */');
-  }
+   headers = [
+     {
+       text: 'Номер',
+       value: 'docNum',
+     },
+     {
+       text: 'Наименование',
+       value: 'name',
+     },
+     {
+       text: 'Дата',
+       value: 'docDate',
+     },
+     {
+       text: 'Срок действия документа',
+       value: 'docEndDate',
+     },
+     {
+       text: 'Группа',
+       value: 'docGroup.name',
+     },
+     {
+       text: 'Примечание',
+       value: 'note',
+     },
+     {
+       text: 'Действия',
+       value: 'actions',
+     },
+   ];
 
-  saveAllInfo() {
-    console.log('goToConclusionsRegistry/*  */');
-  }
+   putOnRecord() {
+     console.log('goToConclusionsRegistry/*  */');
+   }
 
-  cancel() {
-    console.log('goToConclusionsRegistry/*  */');
-  }
+   onDeregister() {
+     console.log('goToConclusionsRegistry/*  */');
+   }
+
+   saveDataApplicant() {
+     console.log('goToConclusionsRegistry/*  */');
+   }
+
+   saveAllInfo() {
+     console.log('goToConclusionsRegistry/*  */');
+   }
+
+   cancel() {
+     console.log('goToConclusionsRegistry/*  */');
+   }
+
+   handleDeleteUD(measureDeleteId: number) {
+     console.log(measureDeleteId);
+
+     this.isDeleteMeasureDialogShow = true;
+   }
+
+   handleDeleteMeasureSuccess() {
+   /*  const { measureDeleteId } = this;
+
+    if (measureDeleteId) {
+      await this.store.measure.deleteMeasureData(measureDeleteId);
+
+      this.fetchMeasures();
+      this.measureDeleteId = null;
+    } */
+   }
 }
 </script>
 
--- src/components/AccountingBusiness/AccountingBusinessList.vue
+++ src/components/AccountingBusiness/AccountingBusinessList.vue
@@ -1,26 +1,21 @@
 <template>
   <main-layout title="Список учетных дел">
     <v-col>
-      <data-table>
-        <template
-          #[`tabs.after`]
-        >
+      <data-table :headers="headers" :items="measuresItems">
+        <template #[`tabs.after`]>
           <v-row>
-            <v-col
-              cols="12"
-              class="d-flex align-start"
-            >
+            <v-col cols="12" class="d-flex align-start">
               <filter-component
                 @onSearch="handleSearch"
                 @onReset="handleReset"
+                :filters="filters"
+                :initial-items="items"
+                eager
               />
             </v-col>
           </v-row>
           <v-row class="mb-8">
-            <v-col
-              cols="12"
-              class="d-flex align-start"
-            >
+            <v-col cols="12" class="d-flex align-start">
               <columns-view @saveColumns="saveColumns" />
               <icon-button
                 @click="handleExportMeasuresInXlsx"
@@ -41,8 +36,58 @@
             </v-col>
           </v-row>
         </template>
+        <template #[`item.actions`]="{ item }">
+          <div class="d-flex justify-center flex-nowrap">
+            <span class="table-action__wrapper">
+              <router-link
+                :to="{
+                  name: 'accountingBusiness-card',
+                  params: { id: item.id, type: 'edit' },
+                }"
+              >
+                <base-action
+                  @click="handleOpenMeasure({ id: item.id })"
+                  hint="Редактировать"
+                >
+                  <edit-icon />
+                </base-action>
+              </router-link>
+            </span>
+            <span class="table-action__wrapper">
+              <base-action @click="handleDeleteMeasure()" hint="Удалить">
+                <delete-icon />
+              </base-action>
+            </span>
+            <span class="table-action__wrapper">
+              <router-link
+                :to="{
+                  name: 'accountingBusiness-card',
+                  params: { id: item.id, type: 'show' },
+                }"
+              >
+                <base-action @click="handleOpenMeasure()" hint="Просмотр">
+                  <eye-icon />
+                </base-action>
+              </router-link>
+            </span>
+          </div>
+        </template>
       </data-table>
     </v-col>
+    <dialog-component
+      v-model="isDeleteMeasureDialogShow"
+      @onSuccess="handleDeleteMeasureSuccess"
+      cancel-title="Отменить"
+      confirm-title="Продолжить"
+      width="600"
+      prompt
+    >
+      <template #title>
+        <text-component variant="h4">
+          Запись будет удалена. Вы уверены?
+        </text-component>
+      </template>
+    </dialog-component>
   </main-layout>
 </template>
 
@@ -55,6 +100,12 @@ import InputComponent from '../shared/inputs/InputComponent.vue';
 import ColumnsView from '../shared/table/ColumnsView/ColumnsView.vue';
 import DataTable from '../shared/table/DataTable.vue';
 import MainLayout from '@/layouts/MainLayout.vue';
+import DeleteIcon from '@/components/shared/IconComponent/icons/DeleteIcon.vue';
+import EditIcon from '@/components/shared/IconComponent/icons/EditIcon.vue';
+import EyeIcon from '@/components/shared/IconComponent/icons/EyeIcon.vue';
+import BaseAction from '@/components/shared/table/RowActions/BaseAction.vue';
+import DialogComponent from '@/components/shared/Dialog/Dialog.vue';
+import TextComponent from '@/components/shared/Text/Text.vue';
 
 @Component({
   name: 'accountingBusinessList',
@@ -66,12 +117,38 @@ import MainLayout from '@/layouts/MainLayout.vue';
     ColumnsView,
     IconButton,
     MainLayout,
+    DeleteIcon,
+    EditIcon,
+    EyeIcon,
+    BaseAction,
+    DialogComponent,
+    TextComponent,
   },
 })
 export default class AccountingBusinessList extends Vue {
   searchName = '';
   fields = ['name', 'dsdf', 'sdfsf'];
-  get filters(): unknown {
+  items = [{
+    items: [
+      {
+        text: 'id',
+        value: '1',
+        code: '1',
+      },
+    ],
+    /* isCustom: 'true',
+    isDefault: 'true',
+    label: 'string',
+    like: 'true',
+    name: 'string',
+    names: 'string',
+    negative: 'true',
+    order: '2', */
+  }];
+
+  isDeleteMeasureDialogShow = false;
+
+  get filters() {
     return {
       multiFilters: [
         {
@@ -83,16 +160,183 @@ export default class AccountingBusinessList extends Vue {
           defaultValue: '',
         },
       ],
-      simpleFilters: [
-        {
-          defaultValue: '',
-          isDefault: true,
-          label: 'Наименование',
-          name: 'name',
-        },
-      ],
     };
   }
+
+  get measuresItems() {
+    return [
+      {
+        id: 13,
+        regDate: '2011-11-11', // дата постановки на учет
+        area: 33, // размер общей площади
+        applicant: { // для фио
+          id: 1,
+          surname: 'testovich',
+          name: 'test',
+          patronymic: null,
+          fullName: 'testovich test',
+        },
+        improvingWay: { // способ улучшения
+          id: 1,
+          code: '1  ',
+          name: 'социальная выплата',
+        },
+        employment: { // сфера деяятельности
+          id: 1,
+          code: '1',
+          name: 'АПК/Ветеринария',
+        },
+        queuePriority: { // приоритет
+          id: 1,
+          code: '1',
+          name: 'Многодетная семья',
+        },
+        familyMembers: [],
+        documents: [],
+        spendingDirection: {
+          id: 1,
+          code: '1',
+          name: 'строительство',
+        },
+        oktmo: {
+          id: 1,
+          code: null,
+          name: null,
+          areaName: null,
+          createDate: null,
+          updateDate: null,
+          regioncode: null,
+          areacode: null,
+          citycode: null,
+          localcode: null,
+          controlnum: null,
+          section: null,
+          clarification: null,
+          lastChangeNum: null,
+          lastChangeType: null,
+        },
+        createDate: '2021-11-23T13:56:14.80455',
+        createUser: null,
+        changeUser: null,
+        changeDate: null,
+        changeReason: null,
+        lkSubId: 1,
+        status: { // статус
+          id: 1,
+          name: 'Черновик',
+        },
+      },
+    ];
+  }
+
+  headers = [
+    {
+      text: 'Фамилия Имя Отчество',
+      value: 'applicant.fullName',
+    },
+    {
+      text: 'Способ улучшения ЖУ',
+      value: 'improvingWay.name',
+    },
+    {
+      text: 'Сфера деятельности',
+      value: 'employment.name',
+    },
+    {
+      text: 'Приоритет',
+      value: 'queuePriority.name',
+    },
+    {
+      text: 'Размер общей площади',
+      value: 'area',
+    },
+    {
+      text: 'Расчетный объем средств',
+      value: 'amount_funds',
+    },
+    {
+      text: 'Дата постановки на учет',
+      value: 'regDate',
+    },
+    {
+      text: 'Статус',
+      value: 'status.name',
+    },
+    {
+      text: 'Действия',
+      value: 'actions',
+    },
+  ];
+
+  columns = [
+    {
+      isDefault: false,
+      text: 'РФ/Субъект РФ',
+      value: 'oktmoName',
+    },
+    {
+      isDefault: false,
+      text: 'ГРБС',
+      value: 'headName',
+    },
+    {
+      isDefault: false,
+      text: 'Рз и ПР',
+      value: 'sectionName',
+    },
+    {
+      isDefault: false,
+      text: 'П',
+      value: 'programName',
+    },
+    {
+      isDefault: false,
+      text: 'ПП',
+      value: 'subprogramName',
+    },
+    {
+      isDefault: false,
+      text: 'М',
+      value: 'mainMeasureName',
+    },
+    {
+      isDefault: false,
+      text: 'НР',
+      value: 'costDirectionName',
+    },
+    {
+      isDefault: false,
+      text: 'ВР',
+      value: 'costTypeName',
+    },
+    {
+      isDefault: true,
+      isEditable: false,
+      text: 'КБК',
+      value: 'kbkCode',
+    },
+    {
+      isDefault: true,
+      isEditable: false,
+      text: 'Наименование',
+      value: 'name',
+    },
+    {
+      isDefault: true,
+      isEditable: false,
+      text: 'Статус',
+      value: 'statusName',
+    },
+    {
+      value: 'actions',
+      text: 'Действия',
+      sortable: false,
+      isVisible: false,
+      isEditable: false,
+      isDefault: true,
+      align: 'center',
+    },
+  ];
 
   handleSearch() {
     console.log('handleSearch');
@@ -110,6 +354,23 @@ export default class AccountingBusinessList extends Vue {
     console.log('handleResetSearch');
   }
 
+  handleDeleteMeasure(measureDeleteId: number) {
+    console.log(measureDeleteId);
+
+    this.isDeleteMeasureDialogShow = true;
+  }
+
+  handleDeleteMeasureSuccess() {
+    /*  const { measureDeleteId } = this;
+
+    if (measureDeleteId) {
+      await this.store.measure.deleteMeasureData(measureDeleteId);
+
+      this.fetchMeasures();
+      this.measureDeleteId = null;
+    } */
+  }
+
   handleOpenMeasure() {
     console.log('handleOpenMeasure');
   }
@@ -121,7 +382,7 @@ export default class AccountingBusinessList extends Vue {
 </script>
 
 <style scoped lang="scss">
-  .table-action__wrapper {
-    width: 28px;
-  }
+.table-action__wrapper {
+  width: 28px;
+}
 </style>
